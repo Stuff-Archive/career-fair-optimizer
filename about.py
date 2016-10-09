7a105d927331f899fb623dc1f334909e29e8daae
SupaStuff <StuffQilla@gmail.com>
2016-10-09 07:35:35 -0400

    finished about.py

--- about.py
+++ about.py
@@ -14,18 +14,20 @@ def api_request_generate(params):
     print(prefix[:-1])
     return prefix[:-1]
 
-
-def getCompanyList():
-
+def getCompanies():
     prams = {"v":1, "format":'json', "t.p": 99384, "t.k":"bvReWCBITHk", "userip":"0.0.0.0", "useragent":"Mozilla/%2F4.0", "action":"employers"}
     url = api_request_generate(prams)
     user_agent = {'User-agent': 'Mozilla/5.0'} #useragent workaroudn
 
     r = requests.get(url, headers=user_agent)
-    json_data = json.loads(r.text)
+    return json.loads(r.text)
+
+
+
+def getCompanyList():
 
     pp = pprint.PrettyPrinter(indent=4)
-    theList = [x['name'] for x in json_data['response']['employers']]
+    theList = [x['name'] for x in getCompanies()['response']['employers']]
 
     for c in theList:
         details.objects.get_or_create(company_name = c)
