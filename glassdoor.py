6a84c29dd86fc7dad0baf58b12d66b11b9ee47af
cartoonman <carl.colena@gmail.com>
2016-10-09 01:56:49 -0400

    first edition of glassdoor api interface with tests

--- glassdoor.py
+++ glassdoor.py
@@ -7,16 +7,19 @@ api_prefix = "http://api.glassdoor.com/api/api.htm?"
 
 class GetCompany:
     params = {"v": 1, "format": 'json', "t.p": 99384, "t.k": "bvReWCBITHk", "userip": "0.0.0.0",
-             "useragent": "Mozilla/%2F4.0", "action": "employers", "q": "nvidia"}
+             "useragent": "Mozilla/%2F5.0", "action": "employers", "q": "nvidia"}
     #gd = None
 
     def __init__(self, company):
         self.params['q'] = company
         user_agent = {'User-agent': 'Mozilla/5.0'}
         url = self.api_request_generate()
-        resp = requests.get(url, headers=user_agent)
+        resp = requests.get(url, headers = user_agent)
+        print(resp.text)
         decoder = json.JSONDecoder()
         self.gd = decoder.decode(resp.text)
+        self.response = self.gd['response']
+        self.employers = self.response['employers']
 
     def api_request_generate(self):
         prefix = api_prefix
@@ -38,19 +41,76 @@ class GetCompany:
         """Returns json response from server"""
         return self.gd['response']
 
+    def is_exact_match(self):
+        return self.employers[0]['exactMatch']
+
+    def get_company(self):
+        return self.employers[0]
+
+class Company:
+    def __init__(self, company):
+        self.comp = company
+
+    def get_name(self):
+        return self.comp['name']
+
+    def get_website(self):
+        return self.comp['website']
+
+    def get_industry(self):
+        return self.comp['industry']
+
+    def get_overallrating(self):
+        return self.comp['overallRating']
+
+    def get_cultureandvaluesrating(self):
+        return self.comp['cultureAndValuesRating']
+
+    def get_leadershiprating(self):
+        return self.comp['seniorLeadershipRating']
+
+    def get_compensationrating(self):
+        return self.comp['compensationAndBenefitsRating']
+
+    def get_workliferating(self):
+        return self.comp['workLifeBalanceRating']
+
+    def get_sectorname(self):
+        return self.comp['sectorName']
+
+    def get_logo(self):
+        return self.comp['squareLogo']
+
+    def get_featuredreview(self):
+        return self.comp['featuredReview']
+
+    def get_ceo(self):
+        return self.comp['ceo']
+
+
 
 def main():
 
-    new = GetCompany("nvidia")
-    old = GetCompany("ibm")
+    n = GetCompany("nvidia")
 
-    print(new.get_status())
-    print(new.get_response())
-    print(old.get_response())
-    #print(test['name'])
-    #print(doctest.title())
-    #print(html)
+    print(n.get_response())
+    print(n.get_status())
+    print(n.is_exact_match())
 
+    c = Company(n.get_company())
+
+    print(c.get_name())
+    print(c.get_overallrating())
+    print(c.get_industry())
+    print(c.get_sectorname())
+    print(c.get_compensationrating())
+    print(c.get_cultureandvaluesrating())
+    print(c.get_leadershiprating())
+    print(c.get_workliferating())
+    print(c.get_website())
+    print(c.get_logo())
+    print(c.get_featuredreview())
+    print(c.get_ceo())
 
 
 
