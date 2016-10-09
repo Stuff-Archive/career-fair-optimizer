a7efba2497277dd2693c4c03e95d5c715201e130
Anthony Shalagin <anthony.shalagin@gmail.com>
2016-10-09 06:40:27 -0400

    temporary

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
+from __future__ import unicode_literals
+
 from django.db import models
+
+#resume, OCI, About, industry, Bulma (alternative to bootstrap)
+
+
+# class home(models.Model):
+#     about = model.TextField(default = "")
+#     OCI = model.BooleanField(default="")
+#     industry = model.TextField(default="")
+#     def __unicode__(self):
+#         return self.about
 
 # Create your models here.
